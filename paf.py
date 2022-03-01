9169f2ef51a66017bc779375287a9949ea0cf2b2
Stephan Scholz <stephan.scholz@hhu.de>
2022-03-01 14:48:32 +0100

    initial commit

---
+++ paf.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+PafEntry = namedtuple('PafEntry', 'qname, qlen, qstart, qend, strand, tname, tlen, tstart, tend, nresiduematch, alignlen, quality, freeinfo')
+
+def parsePafEntry(line: str) -> PafEntry:
+    spl = line.split(maxsplit=12)
+    if len(spl) < 12:
+        print("skipping line " + line)
+        parsed = ParfEntry()
+    parsed = PafEntry(spl[0], int(spl[1]), int(spl[2]), int(spl[3]), spl[4], spl[5], int(spl[6]), int(spl[7]), int(spl[8]), int(spl[9]), int(spl[10]), int(spl[11]), spl[12])
+    return parsed
